e93c5520b79c63c89d8afcf6e873c72b4bbd6747
Jochen1980 <info@jochen-bauer.net>
2013-07-28 14:12:30 +0100

    action 1

---
+++ admin/action1.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<hr><p><a href='index.php' title='Cockpit Start">Zurueck</a></p>";
+
+?>
--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,9 @@
 <?php
 
 // URL: http://localhost/jobascripts/admin/index.php
-echo "Hallo Welt";
+// Web: http://dev.jochen-bauer.net/jobascripts/admin/
+// Verzeichnisschutz via htaccess angelegt, siehe Admintool des Providers;
 
+echo "<h1>Cockpit</h1>";
+echo '<p><a href="action1.php" title="Action 1">Action-1</a></p>';
 ?>
